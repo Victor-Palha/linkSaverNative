424b5162b84fcf6d795476c8be79798e5e54f1c3
strahd <jv.palha@gmail.com>
2024-11-05 15:07:39 -0300

    style: add tailwind css to react native I really do not like using pure CSS when i'm creating new apps, so lets use tailwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text } from "react-native";
 
 export default function Index(){
     return (
-        <Text>
+        <Text className="text-2xl text-blue-950 mt-20 mx-10">
             Hello World, i'm a React Native App
         </Text>
     )
