250b86bfc7c4a6e2082735e828cd633bdbbbdbe4
strahd <jv.palha@gmail.com>
2024-11-06 11:57:26 -0300

    add: navigate between screens

--- src/app/Index/index.tsx
+++ src/app/Index/index.tsx
@@ -4,6 +4,7 @@ import { colors } from "@/src/styles/colors";
 import { Categories } from "@/src/components/categories";
 import { Link } from "@/src/components/link";
 import { Option } from "@/src/components/option";
+import { router } from "expo-router";
 export default function Index(){
     return (
         <View className="flex flex-1 pt-[62]">
@@ -12,7 +13,7 @@ export default function Index(){
                     className="w-[38] h-[32]"
                     source={require("../../assets/logo.png")} 
                 />
-                <TouchableOpacity>
+                <TouchableOpacity onPress={()=> router.navigate("/add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]}/>
                 </TouchableOpacity>
             </View>
@@ -31,7 +32,7 @@ export default function Index(){
                 contentContainerClassName="gap-[20] p-[24] pb-[100]"
             />
 
-            <Modal transparent visible={true}>
+            <Modal transparent visible={false}>
                 <View className="flex-1 justify-end"> 
                     <View className="bg-gray-900 border-t-2 border-gray-800 pb-[52] p-[24]"> 
                         <View className="w-full flex-row justify-between items-center mb-[32]"> 
--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,12 +1,14 @@
+import { Categories } from "@/src/components/categories";
 import { colors } from "@/src/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Add(){
     return (
         <View className="flex-1 pt-[62]">
             <View className="flex-row justify-between px-[24] mb-[24]">
-                <TouchableOpacity>
+                <TouchableOpacity onPress={()=>router.back()}>
                     <MaterialIcons name="arrow-back-ios" size={32} color={colors.gray[200]} />
                 </TouchableOpacity>
                 <Text className="text-2xl font-bold text-gray-200">New</Text>
@@ -15,6 +17,7 @@ export default function Add(){
             <Text className="text-gray-400 text-sm px-[24]">
                 Select a category
             </Text>
+            <Categories/>
         </View>
     )
 }
