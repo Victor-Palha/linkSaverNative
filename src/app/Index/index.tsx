8cd723ddc7263629b6d27513a42f17871c6929d7
strahd <jv.palha@gmail.com>
2024-11-06 08:39:02 -0300

    add: header to app

--- src/app/Index/index.tsx
+++ src/app/Index/index.tsx
@@ -1,9 +1,18 @@
-import { Text } from "react-native";
-
+import { Image, View, TouchableOpacity } from "react-native";
+import {MaterialIcons} from "@expo/vector-icons"
+import { colors } from "@/src/styles/colors";
 export default function Index(){
     return (
-        <Text className="text-1xl text-gray-800 mt-20 mx-10">
-            Hello World, i'm a React Native App
-        </Text>
+        <View className="flex flex-1 pt-[62]">
+            <View className="pl-[24] flex-row justify-between items-center mb-[32]">
+                <Image
+                    className="w-[38] h-[32]"
+                    source={require("../../assets/logo.png")} 
+                />
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]}/>
+                </TouchableOpacity>
+            </View>
+        </View>
     )
 }
