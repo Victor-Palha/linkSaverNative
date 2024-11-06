03a1b25c73ee01b9dca772e8508c9a2afa1be364
strahd <jv.palha@gmail.com>
2024-11-06 10:31:34 -0300

    add: modal to links

--- src/app/Index/index.tsx
+++ src/app/Index/index.tsx
@@ -1,8 +1,8 @@
-import { Image, View, TouchableOpacity } from "react-native";
+import { Image, View, TouchableOpacity, FlatList, Modal, Text } from "react-native";
 import {MaterialIcons} from "@expo/vector-icons"
 import { colors } from "@/src/styles/colors";
-import { Category } from "@/src/components/category";
 import { Categories } from "@/src/components/categories";
+import { Link } from "@/src/components/link";
 export default function Index(){
     return (
         <View className="flex flex-1 pt-[62]">
@@ -17,6 +17,34 @@ export default function Index(){
             </View>
 
             <Categories/>
+            <FlatList
+                data={[1, 2, 3, 4, 5]}
+                keyExtractor={item => item.toString()}
+                renderItem={({ item }) => (
+                    <Link name="Google" link="https://www.google.com" onDetails={() => {
+                        console.log("Details")
+                    }}/>
+                )}
+                showsVerticalScrollIndicator={false}
+                className="border-t-2 border-gray-600"
+                contentContainerClassName="gap-[20] p-[24] pb-[100]"
+            />
+
+            <Modal transparent visible={true}>
+                <View className="flex-1 justify-end"> 
+                    <View className="bg-gray-900 border-t-2 border-gray-800 pb-[52] p-[24]"> 
+                        <View className="w-full flex-row justify-between items-center mb-[32]"> 
+                            <Text className="text-base font-semibold text-gray-400">Modal Header</Text>
+                            <TouchableOpacity>
+                                <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text className="text-lg font-bold text-gray-200">Modal Name</Text>
+                        <Text className="text-sm text-gray-400">Modal Link</Text>
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
