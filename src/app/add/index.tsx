0b88fa98a72fff27b9ffd447ab50adc82d6a1ecc
strahd <jv.palha@gmail.com>
2024-11-06 11:51:31 -0300

    add: new screen to add link

---
+++ src/app/add/index.tsx
@@ -0,0 +1,20 @@
+import { colors } from "@/src/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function Add(){
+    return (
+        <View className="flex-1 pt-[62]">
+            <View className="flex-row justify-between px-[24] mb-[24]">
+                <TouchableOpacity>
+                    <MaterialIcons name="arrow-back-ios" size={32} color={colors.gray[200]} />
+                </TouchableOpacity>
+                <Text className="text-2xl font-bold text-gray-200">New</Text>
+            </View>
+
+            <Text className="text-gray-400 text-sm px-[24]">
+                Select a category
+            </Text>
+        </View>
+    )
+}
